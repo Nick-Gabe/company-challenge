9ad48c9f2ebc4e4ad9f780aaa377548c74d66a29
Nick Gabe <nicolasgabrielctt@gmail.com>
2023-09-26 03:02:17 -0300

    fix(front): header is now responsive

--- apps/frontend/src/entities/Header/ui/Header.tsx
+++ apps/frontend/src/entities/Header/ui/Header.tsx
@@ -14,16 +14,16 @@ export const Header: FC<HeaderProps> = ({ diagramRef }) => {
     <div
       className={`navbar fixed top-0 z-10 ${
         isDark ? "bg-slate-800 text-white" : "bg-gray-300 text-black"
-      } grid grid-cols-3 w-full align-middle px-6`}
+      } grid grid-cols-1 sm:grid-cols-3 w-full align-middle px-6 gap-4`}
     >
-      <div className="flex gap-2">
+      <div className="flex gap-2 columns-1 justify-center sm:justify-start">
         <img className="h-10" src="logo.svg" alt="Logo of a diagram" />
         <h1>Diagram Decision Maker</h1>
       </div>
-      <div className="flex justify-center">
+      <div className="flex justify-center text-center sm:row-auto sm:col-auto">
         <p>Untitled</p>
       </div>
-      <div className="flex gap-2 justify-end">
+      <div className="flex gap-2 justify-center sm:justify-end row-span-1 col-span-1">
         <button
           className={`btn ${isDark ? "bg-gray-500" : "bg-gray-400"} p-1`}
           onClick={switchTheme}
